fab3690c4fce32355c92e6dc3582610d98da1cf6
retr0lbb <retr0lbb@gmail.com>
2025-06-17 13:25:52 -0300

    feat: add multiple pallets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use image::GenericImageView;
 use colored::*;
 use std::{env, error::Error};
+use rand::Rng;
 
-fn get_str_ascii(intent: u8) -> &'static str {
-    let index = intent / 32;
-    let ascii = [" ", ".", ",", "-", "~", "+", "=", "@", "#", "$", "%", "&", "8", "B", "M", "W"];
-    return ascii[index as usize];
+const ASCII_PALETTE_1: [&str; 16] = [" ", ".", ",", "-", "~", "+", "=", "@", "#", "$", "%", "&", "8", "B", "M", "W"];
+const ASCII_PALETTE_2: [&str; 16] = [" ", ":", ";", "!", "^", "*", "x", "o", "O", "0", "Q", "X", "H", "M", "W", "@"];
+
+
+fn get_str_ascii<'a>(intent: u8, palette: &'a [&'a str; 16]) -> &'a str {
+    let index = (intent / 16).min(15);
+    palette[index as usize]
 }
  
 
@@ -27,6 +31,8 @@ fn get_image(path_or_url: &str, scale: u32) {
 
             for y in 0..height{
                 for x in 0..width{
+                    let mut rng = rand::thread_rng();
+                    let pallet_choice = if rng.gen_bool(0.5) {&ASCII_PALETTE_1} else {&ASCII_PALETTE_2};
                     if y % (scale * 2) == 0 && x % scale == 0{
                         let pix = img.get_pixel(x, y);
                         let mut intent = pix[0]/3 + pix[1]/3 + pix[2]/3;
@@ -38,7 +44,7 @@ fn get_image(path_or_url: &str, scale: u32) {
                             intent = 0;
                         }
 
-                        let ascii_char = get_str_ascii(intent);
+                        let ascii_char = get_str_ascii(intent, pallet_choice);
                         print!("{}", ascii_char.truecolor(r, g, b))
                     }
                 }
@@ -56,12 +62,14 @@ fn get_image(path_or_url: &str, scale: u32) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len()<2{
-        eprint!("Uso: cargo run <caminho_ou_url_da_imagem> <escala_reduzida>");
+    if args.len()<3{
+        eprint!("Uso: cargo run <caminho_ou_url_da_imagem> <escala_reduzida> <colorido?>");
         return;
     }
     let path_or_url = &args[1];
     let scale = args.get(2).unwrap_or(&"6".to_string()).parse::<u32>().unwrap_or(6);
+    let is_colored = args.get(3).unwrap();
+
 
     get_image(path_or_url, scale);
 }
